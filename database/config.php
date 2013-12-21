427341fe2b6f8be07827a4247a7d5a474bf841af
Gemin <geminantoine@gmail.com>
2013-12-22 00:21:25 +0100

    db update (delete dbtest + delete false merging)

--- database/config.php
+++ database/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 return array
@@ -11,17 +10,5 @@ return array
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
 	)
 );
-=======
-<?php
 
-return array
-(
-	'dsn' => 'mysql:dbname=topuv;host=localhost',
-	'username' => 'root',
-	'password' => '',
-	'options' => array
-	(
-		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
-	)
-);
->>>>>>> branch 'master' of https://github.com/repere/TopUV.git
+?>
