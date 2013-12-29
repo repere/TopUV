18be7c1e8a62f7f78e8ccf26d5298290b9f53b5f
Rlange <Rlange@Mini-PC>
2013-12-29 21:59:13 +0100

    delete useless menu xml
    add back home option on UvCommentActivity

--- src/fr/utt/topuv/activity/UvCommentActivity.java
+++ src/fr/utt/topuv/activity/UvCommentActivity.java
@@ -9,7 +9,11 @@ package fr.utt.topuv.activity;
 import fr.utt.topuv.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class UvCommentActivity extends Activity {
 
@@ -17,13 +21,42 @@ public class UvCommentActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_uv_comment);
+		
+		// Specify that the Home button should show an "Up" caret, indicating that touching the
+        // button will take the user one step up in the application's hierarchy.
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.uv_comment, menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This is called when the Home (Up) button is pressed in the action bar.
+                // Create a simple intent that starts the hierarchical parent activity and
+                // use NavUtils in the Support Package to ensure proper handling of Up.
+                Intent upIntent = new Intent(this, UvActivity.class);
+                if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
+                    // This activity is NOT part of this app's task, so create a new task
+                    // when navigating up, with a synthesized back stack.
+                    TaskStackBuilder.create(this)
+                            // Add all of this activity's parents to the back stack
+                            .addNextIntentWithParentStack(upIntent)
+                            // Navigate up to the closest parent
+                            .startActivities();
+                } else {
+                    // This activity is part of this app's task, so simply
+                    // navigate up to the logical parent activity.
+                    NavUtils.navigateUpTo(this, upIntent);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
