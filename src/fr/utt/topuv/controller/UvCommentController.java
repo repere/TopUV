7e22f56316ab586638a1fec1b1487f6e56417cfa
Rlange <Rlange@192.168.1.77>
2014-01-13 17:51:00 +0100

    add verification before sending comment

--- src/fr/utt/topuv/controller/UvCommentController.java
+++ src/fr/utt/topuv/controller/UvCommentController.java
@@ -9,7 +9,9 @@ import fr.utt.topuv.service.PutCommentService;
 import fr.utt.topuv.R;
 
 import android.os.Bundle;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,9 @@ public class UvCommentController extends Fragment implements OnClickListener
 {
 	private String code;
 	private int idUser;
+	private String note;
+	private String idUserToString;
+	private String comment;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -45,15 +50,15 @@ public class UvCommentController extends Fragment implements OnClickListener
 		idUser = this.getActivity().getIntent().getIntExtra(IntentConstants.ID_USER, 0);
 		
 		//Convert the idUser int to String to due AsyncTask restriction (only accept one type, here String)
-		String idUserToString = String.valueOf(idUser);
+		idUserToString = String.valueOf(idUser);
     	
     	RadioGroup radioGroup = (RadioGroup) getActivity().findViewById(R.id.radioGroup1);
     	int selectedId = radioGroup.getCheckedRadioButtonId();
     	RadioButton radioButtonSelected = (RadioButton) getActivity().findViewById(selectedId);
-    	String note = (String) radioButtonSelected.getText();
+    	note = (String) radioButtonSelected.getText();
     	note = note.substring(0, 1);
     	
-        String comment = ((EditText) this.getView().findViewById(R.id.comment_to_add)).getText().toString();
+        comment = ((EditText) this.getView().findViewById(R.id.comment_to_add)).getText().toString();
         
         // Basic local validation
         boolean error = false;
@@ -63,12 +68,37 @@ public class UvCommentController extends Fragment implements OnClickListener
             error = true;
         }
         
+        if(comment.length() < 2)
+        {
+            ((EditText) this.getView().findViewById(R.id.comment_to_add)).setError(this.getString(R.string.comment_length));
+            error = true;
+        }
+        
         if(error)
         {
             return;
         }
         
-        PutCommentService putCommentService = new PutCommentService(getActivity());
-        putCommentService.execute(idUserToString, code, comment, note);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_message);
+        builder.setIcon(R.drawable.ic_action_warning);
+        builder.setTitle(R.string.dialog_title);
+        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() 
+               {
+                   public void onClick(DialogInterface dialog, int id) 
+                   {
+                	   PutCommentService putCommentService = new PutCommentService(getActivity());
+                       putCommentService.execute(idUserToString, code, comment, note);
+                   }
+               });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() 
+               {
+                   public void onClick(DialogInterface dialog, int id) 
+                   {
+                	   dialog.dismiss();
+                   }
+               });
+
+        builder.show();
     }
 }
